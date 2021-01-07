f66c4ee2fa9ad65a485bef913beb13d83f563691
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2021-01-07 14:36:39 -0500

    Add files via upload

---
+++ Console.js
@@ -0,0 +1,14 @@
+class Console {
+
+    constructor() {
+
+
+        console.log('Loaded Files\n-----------------------------\n')
+
+        console.info('Loaded Client\n-----------------------------\n')
+
+        console.info('Loaded CommandHandler\n-----------------------------\n')
+    }
+}
+
+module.exports = Console;
---
+++ Index.js
@@ -0,0 +1,9 @@
+const karo = require('./KaroClient');
+const Command = require('./Command');
+const console = require('./Console');
+
+//------------------------------//
+new karo()
+new Command()
+new console()
+//----------------------------//
