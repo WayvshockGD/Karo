1b50d078c699e1aacb5bc8bc27b9291398570f4f
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2021-01-08 22:54:16 -0500

    Add files via upload

--- Index.js
+++ Index.js
@@ -1,9 +1,11 @@
-const karo = require('./KaroClient');
+const karo = require('./src/KaroClient');
 const Command = require('./Command');
-const console = require('./Console');
+const console = require('./src/Console');
+const DataBase = require('./src/Database');
 
 //------------------------------//
 new karo()
 new Command()
 new console()
+new DataBase()
 //----------------------------//
